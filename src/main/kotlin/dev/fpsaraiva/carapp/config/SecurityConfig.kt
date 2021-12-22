02b88ae18a515967f191820c3b64aa66898b4d07
Fernando Saraiva <fpsaraiva@gmail.com>
2021-12-22 17:49:54 -0300

    implementando autorização de usuários

--- src/main/kotlin/dev/fpsaraiva/carapp/config/SecurityConfig.kt
+++ src/main/kotlin/dev/fpsaraiva/carapp/config/SecurityConfig.kt
@@ -1,13 +1,17 @@
 package dev.fpsaraiva.carapp.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.userdetails.User
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
 class SecurityConfig: WebSecurityConfigurerAdapter()  {
 
     override fun configure(http: HttpSecurity) {
@@ -22,5 +26,29 @@ class SecurityConfig: WebSecurityConfigurerAdapter()  {
             .authenticated()
             .and()
             .httpBasic()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        val password = "{noop}password"
+
+        val driver = User.builder()
+            .username("driver")
+            .password(password)
+            .roles("DRIVER")
+
+        val passenger = User.builder()
+            .username("passenger")
+            .password(password)
+            .roles("PASSENGER")
+
+        val admin = User.builder()
+            .username("admin")
+            .password(password)
+            .roles("ADMIN")
+
+        auth.inMemoryAuthentication()
+            .withUser(driver)
+            .withUser(passenger)
+            .withUser(admin)
     }
 }
